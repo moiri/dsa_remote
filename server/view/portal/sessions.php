0db78792c4227d81fa97d17dd80df2b63cbccfa9
Simon Maurer <moirelein@gmail.com>
2015-09-17 23:33:36 +0100

    an example as how the session screen could look like

--- server/view/portal/sessions.php
+++ server/view/portal/sessions.php
@@ -3,11 +3,41 @@
         <h1>Portal Sessions</h1>
         <p>Hier werden alle deine Session aufgelistet. Du kannst auch eine neue Session erstellen.</p>
     </div>
-    <div class="list-group">
-        <button type="button" class="list-group-item">Cras justo odio</button>
-        <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
-        <button type="button" class="list-group-item">Morbi leo risus</button>
-        <button type="button" class="list-group-item">Porta ac consectetur ac</button>
-        <button type="button" class="list-group-item">Vestibulum at eros</button>
+<div class="panel panel-default">
+  <div class="panel-body">
+        <div class="list-group">
+            <a href="#" class="list-group-item">
+                <h4 class="list-group-item-heading">Session-Name 1</h4>
+                <p class="list-group-item-text">Beschreibung</p>
+            </a>
+            <a href="#" class="list-group-item">
+                <h4 class="list-group-item-heading">Session-Name 2</h4>
+                <p class="list-group-item-text">Beschreibung</p>
+            </a>
+            <a href="#" class="list-group-item">
+                <h4 class="list-group-item-heading">Session-Name 3</h4>
+                <p class="list-group-item-text">Beschreibung</p>
+            </a>
+        </div>
+        <div class="input-group">
+        <input type="text" class="form-control" placeholder="Name einer neuen Session ...">
+        <span class="input-group-btn">
+            <button class="btn btn-default" type="button">Erstellen</button>
+        </span>
+        </div>
+    </div>
+    </div>
+    <div class="well">
+        <form method="post" name="loginform" action="index.php<?php echo $args;?>">
+            <div class="form-group">
+                <label class="sr-only" for="session_input_name">Name</label>
+                <input id="session_input_name" class="form-control" type="text" placeholder="Name" name="session_name" required />
+            </div>
+            <div class="form-group">
+                <label class="sr-only" for="session_input_desc">Beschreibung</label>
+                <textarea id="session_input_desc" class="form-control" rows="3" placeholder="Beschreibung"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success btn-block" name="login">Erstellen</button>
+        </form>
     </div>
 </div>

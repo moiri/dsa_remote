875a050f90c5461ad86934f08a131783be4ed171
Simon Maurer <moirelein@gmail.com>
2016-07-15 19:58:51 +0100

    some first steps thowards fetching data via js

---
+++ php/get_json.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+ * this script gets informaion from the db
+ * and returns te results as json string
+ */
+
+session_start();
+require_once('../server/class/sheetDbMapper.php');
+require_once('../server/secure/globals.php');
+
+header('Content-Type: text/plain');
+
+$sheet = new SheetDbMapper(DBSERVER,DBNAME,DBUSER,DBPASSWORD);
+$res = false;
+
+switch( $_GET['j'] ) {
+case 'gruppen':
+    $res = $sheet->getGroupsGyUserId($_SESSION['user_id']);
+    break;
+default:
+    $res['error'] = true;
+    $res['val'] = -99;
+}
+
+print json_encode($res);
+
+?>
